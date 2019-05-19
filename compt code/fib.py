b1d55e1fd79cd36dcfcc3473715793dd10929596
lazy_bird <40322924+Aryann-rajnish@users.noreply.github.com>
2019-05-20 00:47:37 +0530

    Add files via upload

---
+++ compt code/fib.py
@@ -0,0 +1,21 @@
+def fib(n):
+ 
+    # array declaration
+    f = [0]*(n+1)
+ 
+    # base case assignment
+    f[1] = 1
+ 
+    # calculating the fibonacci and storing the values
+    for i in xrange(2 , n+1):
+        f[i] = f[i-1] + f[i-2]
+    return f[n]
+ 
+# Driver program to test the above function
+def main():
+    n = 40
+    print "Fibonacci number is " , fib(n)
+ 
+if __name__=="__main__":
+    main()
+
